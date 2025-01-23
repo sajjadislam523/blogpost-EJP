de58e09c19bd8428104f9278b64271385744102d
sajjadislam523 <sajjad.islam523@gmail.com>
2025-01-23 17:38:19 +0600

    Updated login logic and added loading component

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,22 +1,50 @@
 "use client";
 
-import { useKindeAuth } from "@kinde-oss/kinde-auth-nextjs";
+import { LoginLink, useKindeAuth } from "@kinde-oss/kinde-auth-nextjs";
 import { useEffect, useState } from "react";
+import Loading from "../../components/Loading.jsx";
 
 export default function ProfilePage() {
     const { getUser } = useKindeAuth();
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         async function fetchUser() {
-            const fetchedUser = await getUser();
-            setUser(fetchedUser);
+            try {
+                const fetchedUser = await getUser();
+                setUser(fetchedUser);
+            } catch (err) {
+                console.error("Error fetching user:", err.message);
+            } finally {
+                setLoading(false);
+            }
         }
         fetchUser();
     }, [getUser]);
 
-    if (!user) {
-        return <div>Loading...</div>;
+    if (loading) {
+        return <Loading />;
+    }
+
+    if (!loading && !user) {
+        return (
+            <div className="min-h-screen flex items-center justify-center bg-gray-50">
+                <div className="p-8 rounded-lg shadow-lg bg-white">
+                    <h1 className="text-2xl font-bold mb-4">
+                        You are not logged in!
+                    </h1>
+                    <p className="text-gray-600 mb-4">
+                        Please log in to access your profile.
+                    </p>
+                    <LoginLink>
+                        <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+                            Log In
+                        </button>
+                    </LoginLink>
+                </div>
+            </div>
+        );
     }
 
     return (
