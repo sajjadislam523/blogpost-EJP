8963118648eba7c0639d17d99ea03cbefdb32b71
sajjadislam523 <sajjad.islam523@gmail.com>
2025-01-23 19:56:02 +0600

    Added footer from daisyui and resolved login related error after deployment

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer.jsx";
 import Navbar from "@/components/Navbar";
 import { AuthProvider } from "./AuthProvider.jsx";
 import "./globals.css";
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
                 <body>
                     <Navbar />
                     <main>{children}</main>
+                    <Footer />
                 </body>
             </html>
         </AuthProvider>
